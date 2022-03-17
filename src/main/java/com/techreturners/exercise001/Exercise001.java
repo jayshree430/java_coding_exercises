5c7aad3e9f5bd396618b9202ba9250853d69dc5e
jayshree430 <iyer430@gmail.com>
2022-03-17 11:06:34 +0000

    1. Implemented all the functions Exercise001.java file 2.Implemented all the functions in Exercise002.java file. 3. Created class Person to be used in Exercise002 and Exercise002Test files.

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,60 @@
 package com.techreturners.exercise001;
 
+import java.math.RoundingMode;
 import java.util.List;
+import java.math.BigDecimal;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        /*
+        Checking if the first letter of the string is Capital.
+         If not perform the following operations.
+        */
+        if (!(Character.isUpperCase(word.charAt(0)))){
+            return word.substring(0,1).toUpperCase() + word.substring(1);
+        }
+        // The first Letter is already Capital so returning as it is.
+        return word;
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+
+        return String.valueOf(firstName.charAt(0)) + String.valueOf('.') + String.valueOf(lastName.charAt(0));
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+
+        if(vatRate > 0)
+        {
+            /*
+            * Checking to see if vat exists. If yes then calculate vat and add to original price.
+            * */
+            originalPrice += (originalPrice * vatRate)/100;
+        }
+        /*
+        * Converting result into BigDecimal to maintain decimal points up to 2 places without rounding up.
+        * */
+        BigDecimal bd = new BigDecimal(originalPrice).setScale(2, RoundingMode.HALF_DOWN);
+        return bd.doubleValue();
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        StringBuilder reverseString = new StringBuilder(sentence).reverse();
+        return reverseString.toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+
+        int count =0;
+        /*
+        * Iterating through the list to check if type is "Linux". If yes then increase the counter.
+        * */
+        for (User u1 : users) {
+            if (u1.getType().equals("Linux")) {
+                count++;
+            }
+        }
+        return count;
     }
 }
+
