211fe9c78fbe9dd2afd7a36a5f0692885014c1f7
jayshree430 <iyer430@gmail.com>
2022-03-19 11:04:24 +0000

    1. Implemented pangram method in Exercise005.java 2. Created some test cases in Exercise005Test.java

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,28 @@
 package com.techreturners.exercise005;
 
+import java.util.Locale;
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
-    }
+        if (input ==null && input.length()==0) return false;
+        boolean[] alphabetList = new boolean[26];
+        int index = 0;
+        boolean flag = true;
+        input = input.replaceAll("[-+^ 0-9]*", "").toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            if( input.charAt(i) >= 'a' && input.charAt(i) <= 'z') {
+                index = input.charAt(i) - 'a';
+            }
+            alphabetList[index] = true;
+        }
+        for (int i = 0; i <= 25; i++) {
+            if (!alphabetList[i]) {
+                flag = false;
+                break;
+            }
+        }
 
+        return flag;
+    }
 }
